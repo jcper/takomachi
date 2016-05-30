38ef104b607ae0b9cee5f1b62574bf665698755d
jcper <informaticajcper@gmail.com>
2016-05-31 00:17:55 +0200

    commit inicio

---
+++ src/es/takomachi/Mascota.java
@@ -0,0 +1,43 @@
+package es.takomachi;
+
+public class Mascota implements interaccion {
+	
+    private   String  Nombre;                       
+    private   int     Peso;                        
+    private   String  Ropa;
+    private   float   Temperatura;
+    private    int    Energia;
+    private    int    Posicion;
+
+    public Mascota(String Nombre, int Peso, String Ropa, float Temperatura, int Energia, int Posicion){
+    	
+    	
+    	
+    	
+    }
+
+	@Override
+	public void DarComer(String comida) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void PonerRopa(String Ropa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void QuitarRopa(String Ropa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Moverse(int x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
