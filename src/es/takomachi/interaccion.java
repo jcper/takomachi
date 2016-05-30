c97b04419b2ea6faf97915f5bda77a90263a15f7
jcper <informaticajcper@gmail.com>
2016-05-31 00:17:55 +0200

    commit inicio

---
+++ src/es/takomachi/interaccion.java
@@ -0,0 +1,9 @@
+package es.takomachi;
+
+public interface interaccion {
+	
+	void DarComer(String comida);
+    void PonerRopa(String Ropa);
+    void QuitarRopa(String Ropa);
+    void Moverse(int x);
+ }
