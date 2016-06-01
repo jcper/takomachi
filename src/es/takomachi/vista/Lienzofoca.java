f158c53f1b20c198838f3ba507de9fde8c479431
jcper <informaticajcper@gmail.com>
2016-06-02 01:11:52 +0200

    version vista terminada creado canvas perro,foca,gato.

---
+++ src/es/takomachi/vista/Lienzofoca.java
@@ -0,0 +1,59 @@
+package es.takomachi.vista;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Lienzofoca extends JPanel {
+	
+    
+Lienzofoca() {
+	  this.setBackground(Color.yellow);
+	  this.setBounds(0,0,100,200);
+ }
+
+@Override
+public void paint(Graphics r){
+	super.paint(r);
+//Cabeza
+r.setColor(Color.orange);
+r.fillOval(200, 130, 100, 100);
+r.setColor(Color.blue);
+r.fillOval(225, 160, 15, 15);
+r.fillOval(260, 160, 15, 15);
+r.setColor(Color.black);
+r.fillOval(249, 182, 10, 10);
+r.setColor(Color.black);
+r.fillRect(235, 205, 40, 5);
+
+//Cuerpo
+r.setColor(Color.white);
+r.fillOval(175, 230, 150, 150);
+r.setColor(Color.MAGENTA);
+r.fillRect(210, 230, 20, 90);
+r.fillRect(210, 320, 5, 10);
+r.fillRect(217, 320, 5, 10);
+r.fillRect(225, 320, 5, 10);
+
+r.fillRect(270, 230, 20, 90);
+r.fillRect(270, 320, 5, 10);
+r.fillRect(277, 320, 5, 10);
+r.fillRect(285, 320, 5, 10);
+//Manos
+r.setColor(Color.cyan);
+r.fillOval(300, 250, 20, 60);
+r.fillOval(175, 250, 20, 60);
+//Botones
+r.setColor(Color.black);
+r.fillOval(245, 250, 15, 15);
+r.fillOval(245, 280, 15, 15);
+r.fillOval(245, 310, 15, 15);
+r.fillOval(245, 340, 15, 15);
+//Pies
+r.setColor(Color.black);
+r.fillOval(150, 370, 90, 40);
+r.fillOval(260, 370, 90, 40);
+
+  }
+
+}
--- src/es/takomachi/vista/pantalla.java
+++ src/es/takomachi/vista/pantalla.java
@@ -1,9 +1,12 @@
 package es.takomachi.vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
 
 
 public class pantalla extends JFrame  {
@@ -15,6 +18,11 @@ public class pantalla extends JFrame  {
     static private JRadioButton tipomascotaFoca;
     static private JRadioButton tipomascotaPerro;
     static private JRadioButton tipomascotaGato;
+    static private JLabel TipoMascota;
+    static private JLabel Comer;
+    static private JLabel Quitar_Poner;
+    static private JLabel Espacio;
+    static private JLabel Mover;
     static private JComboBox comer;
     static private JComboBox ropa;
     static private JSlider mover;
@@ -25,10 +33,21 @@ public class pantalla extends JFrame  {
     static double temperatura_p;
     static int peso_p;
     static 	ButtonGroup grupo;
+    
+    
+    
     public pantalla(){
    temperatura_p=19.5;
    peso_p=10;
    temperatura=new JLabel ("Temperatura:"+temperatura_p+"º");
+   TipoMascota=new JLabel("TIPO MASCOTA");
+   
+   Comer=new JLabel("COMER");
+   
+   Espacio=new JLabel("");
+   Quitar_Poner=new JLabel("QUITAR/PONER ROPA");
+   Mover=new JLabel("MOVER EJE X");
+  
    energia=new JProgressBar();
    energia.setValue(100);
    energia.setStringPainted(true);
@@ -46,10 +65,13 @@ public class pantalla extends JFrame  {
    panel.add(tipomascotaGato);
    panel.add(tipomascotaFoca);
    comer=new JComboBox();
+   comer.setSize(25,10);
+   comer.addItem("");
    comer.addItem("Carne");
    comer.addItem("Leche");
    comer.addItem("Langosta");
    ropa=new JComboBox();
+   ropa.addItem("");
    ropa.addItem("Poner Camiseta");
    ropa.addItem("Poner Gorro");
    ropa.addItem("Quitar Camiseta");
@@ -64,37 +86,57 @@ public class pantalla extends JFrame  {
    salir=new JButton("Salir Juego");
    interaccion=new JButton("interaccion");
    JFrame ventana = new JFrame("Pantalla");
-   JPanel Pestado=new JPanel(new FlowLayout());
+   JPanel Pestado=new JPanel(new FlowLayout(FlowLayout.CENTER,120,10));
      Pestado.add(temperatura);
      Pestado.add(energia);
      Pestado.add(nombre);
      Pestado.add(peso);
    JPanel Pinteraccion=new JPanel();
+   JPanel Pmascota=new JPanel(new FlowLayout(FlowLayout.CENTER,40,10));
+     Pmascota.add(TipoMascota);
+     Pmascota.add(panel);
+  JPanel Pcomer=new JPanel(new FlowLayout(FlowLayout.CENTER,60,10));
+         Pcomer.add(Comer);
+         Pcomer.add(comer);
+  JPanel Propa=new JPanel(new FlowLayout(FlowLayout.CENTER,40,10));
+         Propa.add(Quitar_Poner);
+         Propa.add(ropa);
+  JPanel Pmover=new JPanel(new FlowLayout(FlowLayout.CENTER,40,10));
+         Pmover.add(Mover);
+         Pmover.add(mover);
         Pinteraccion.setLayout(new BoxLayout(Pinteraccion,BoxLayout. Y_AXIS));
-    	Pinteraccion.add(panel);
-    	Pinteraccion.add(comer);
-    	Pinteraccion.add(ropa);
-    	Pinteraccion.add(mover);
+        Pinteraccion.add(Pmascota);
+    	Pinteraccion.add(Pcomer);
+    	Pinteraccion.add(Propa);
+    	Pinteraccion.add(Pmover);
     
    JPanel Pjuego=new JPanel();
         Pjuego.setLayout(new BoxLayout(Pjuego,BoxLayout. Y_AXIS));
-          Pjuego.add(cargar);
-          Pjuego.add(guardar);
-          Pjuego.add(salir);
+        JPanel Pcargar=new JPanel(new FlowLayout(FlowLayout.CENTER));
+               Pcargar.add(cargar);
+       JPanel Pguardar=new JPanel(new FlowLayout(FlowLayout.CENTER));
+               Pguardar.add(guardar);
+       JPanel Psalir=new JPanel(new FlowLayout(FlowLayout.CENTER));
+               Psalir.add(salir);
+          Pjuego.add(Pcargar);
+          Pjuego.add(Pguardar);
+          Pjuego.add(Psalir);
     
-   JPanel Paccion=new JPanel(new FlowLayout());
+   JPanel Paccion=new JPanel(new FlowLayout(FlowLayout.CENTER,40,10));
           Paccion.add(interaccion);
-      JPanel dibujo=new JPanel();
-       dibujo.setLayout(null);
+    JPanel dibujo=new JPanel();
+      dibujo.setLayout(null);
+	 
           
         ventana.setLayout(new BorderLayout());
       	ventana.add(Pestado,BorderLayout.NORTH);
 		ventana.add(Pjuego,BorderLayout.EAST);
 		ventana.add(Paccion,BorderLayout.SOUTH);
 		ventana.add(Pinteraccion,BorderLayout.WEST);
-		ventana.add(dibujo,BorderLayout.CENTER);
-		ventana.setSize(600,400);		
+		ventana.add(new Lienzoperro(),BorderLayout.CENTER);
+		//ventana.setSize(400,600);	
 		//ventana.pack();
+		ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		ventana.setVisible(true);
     }
     
